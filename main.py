a07c640fc6480d1e916f01626408bcfaf21d6397
PENTASHIFT <jwmunsey1@gmail.com>
2024-07-16 16:04:49 -0700

    split backend to two pages

--- main.py
+++ main.py
@@ -2,7 +2,7 @@ import json
 
 import spotipy
 from spotipy import SpotifyClientCredentials
-from flask import Flask, render_template, request
+from flask import Flask, render_template, request, redirect, url_for
 
 # Spotify setup.
 SECRETS = json.load(open("secrets.json"))
@@ -23,5 +23,9 @@ def index():
         playlist_url = request.form.get("playlist-url")
         playlist_uri = playlist_url.split("/")[-1].split("?")[0]
 
-        tracks = sp.playlist_tracks(playlist_uri)["items"]
-        return render_template("index.html", tracks=tracks)
+        return redirect(url_for("module", uri=playlist_uri))
+
+@app.route("/module/<uri>")
+def module(uri):
+    tracks = sp.playlist_tracks(uri)["items"]
+    return render_template("index.html", tracks=tracks)
